2f8ba0956e9386881de34a84330c6853c92ec9ba
mohsen <mohsen.abdalla.98@gmail.com>
2021-01-05 21:41:13 +0200

    http request

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./Pages/HomePage";
 import NavBar from "./Components/NavBar";
+import ExperimentPage from "./Pages/ExperimentPage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <HomePage />
+      {/* <ExperimentPage /> */}
     </div>
   );
 }
