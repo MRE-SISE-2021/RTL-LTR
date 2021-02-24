e5d13f494ad79717687313968d3215e55f78db2f
mohsen <mohsen.abdalla.98@gmail.com>
2021-02-24 16:15:58 +0200

    preview

--- Front/src/Components/ExperimentInfo.js
+++ Front/src/Components/ExperimentInfo.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Row, Col } from "react-bootstrap";
-import Card from "../App/components/MainCard";
+import { Row, Col, Card } from "react-bootstrap";
+import { MDBIcon } from "mdbreact";
+import { Button } from "react-bootstrap";
+
+// import Card from "../App/components/MainCard";
 import Aux from "../hoc/_Aux";
 import * as actionTypes from "../store/actions";
 class ExperimentInfo extends Component {
@@ -46,35 +49,66 @@ class ExperimentInfo extends Component {
                       <Aux>
                         <Row>
                           <Col>
-                            <Card title={data.questionnaire_name} isOption>
-                              <Row>
-                                <Col>
-                                  <b>Created: </b> {data.creation_date}
-                                </Col>
-                                <Col>
-                                  <b>Language: </b>
-                                  {
+                            <Card isOption>
+                              <Card.Header>
+                                <Card.Title>
+                                  {data.questionnaire_name}
+                                  <div className="d-flex justify-content-lg-end">
+                                    <Button variant="outline-*" disabled>
+                                      <MDBIcon className="mr-5" icon="upload" />
+                                    </Button>
+                                    <Button
+                                      variant="outline-*"
+                                      onClick={this.submitHandler}
+                                    >
+                                      <MDBIcon className="mr-5" icon="eye" />
+                                    </Button>
+
+                                    <Button variant="outline-*" disabled>
+                                      <MDBIcon className="mr-5" icon="clone" />
+                                    </Button>
+                                    <Button variant="outline-*" disabled>
+                                      <MDBIcon className="mr-5" icon="edit" />
+                                    </Button>
+                                    <Button variant="outline-*" disabled>
+                                      <MDBIcon
+                                        className="mr-5"
+                                        icon="trash-alt"
+                                      />
+                                    </Button>
+                                  </div>
+                                </Card.Title>
+                              </Card.Header>
+                              <Card.Body>
+                                <Row>
+                                  <Col>
+                                    <b>Created: </b> {data.creation_date}
+                                  </Col>
+                                  <Col>
+                                    <b>Language: </b>
                                     {
-                                      1: "Arabic",
-                                      2: "English",
-                                      3: "Hebrew",
-                                      4: "Russian",
-                                    }[data.language_id]
-                                  }
-                                </Col>
-                              </Row>
-                              <Row>
-                                <Col>
-                                  <b>Hosted Link: </b>
-                                  {data.hosted_link}
-                                </Col>
-                                <Col>
-                                  <b>Status: </b>
-                                  {data.is_active !== undefined
-                                    ? [data.is_active ? "True" : "False"]
-                                    : null}
-                                </Col>
-                              </Row>
+                                      {
+                                        1: "Arabic",
+                                        2: "English",
+                                        3: "Hebrew",
+                                        4: "Russian",
+                                      }[data.language_id]
+                                    }
+                                  </Col>
+                                </Row>
+                                <Row>
+                                  <Col>
+                                    <b>Hosted Link: </b>
+                                    {data.hosted_link}
+                                  </Col>
+                                  <Col>
+                                    <b>Status: </b>
+                                    {data.is_active !== undefined
+                                      ? [data.is_active ? "True" : "False"]
+                                      : null}
+                                  </Col>
+                                </Row>
+                              </Card.Body>
                             </Card>
                           </Col>
                         </Row>
---
+++ Front/src/Pages/PreviewPage.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Aux from "../hoc/_Aux";
+import * as actionTypes from "../store/actions";
+
+class ExperimentPage extends Component {
+  UNSAFE_componentWillMount() {
+    if (
+      this.props.windowWidth > 992 &&
+      this.props.windowWidth <= 1024 &&
+      this.props.layout !== "horizontal"
+    ) {
+      this.props.onComponentWillMount();
+    }
+  }
+
+  mobileOutClickHandler() {
+    if (this.props.windowWidth < 992 && this.props.collapseMenu) {
+      this.props.onComponentWillMount();
+    }
+  }
+
+  render() {
+    console.log(this.props.match);
+    return (
+      <Aux>
+        <h1>Previw Page!!!!!!!!!!!!!!</h1>
+      </Aux>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    defaultPath: state.defaultPath,
+    collapseMenu: state.collapseMenu,
+    layout: state.layout,
+    fullWidthLayout: state.fullWidthLayout,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onComponentWillMount: () => dispatch({ type: actionTypes.COLLAPSE_MENU }),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExperimentPage);
