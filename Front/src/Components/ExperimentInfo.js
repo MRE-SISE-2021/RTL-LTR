a30b4dcf23a75023e9d175c9c32df477d13f5fb9
mohsen <mohsen.abdalla.98@gmail.com>
2021-03-01 22:27:30 +0200

    homepage css

--- Front/src/Components/ExperimentInfo.js
+++ Front/src/Components/ExperimentInfo.js
@@ -52,127 +52,75 @@ class ExperimentInfo extends Component {
       return <Redirect to={"/preview/" + data.questionnaire_id} />;
     }
 
-    // console.log(this.props.chosen);
-    let mainClass = ["content-main"];
-    if (this.props.fullWidthLayout) {
-      mainClass = [...mainClass, "container-fluid"];
-    } else {
-      mainClass = [...mainClass, "container"];
-    }
-
     console.log(data);
     return (
       <Aux>
         <nav
-          className="p-3 mb-2 bg-info text-white"
-          style={{ height: "100%", marginTop: "53px" }}
-          className={mainClass.join(" ")}
+          className="bg-info text-white"
+          style={{ marginTop: "150px", marginLeft: "200px" }}
         >
-          {/* <NavBar /> */}
+          <Aux>
+            <Card>
+              <Card.Header>
+                <Card.Title>
+                  <b className="text-info">{data.questionnaire_name}</b>
+                  <div className="d-flex justify-content-lg-end">
+                    <Button variant="outline-*" disabled>
+                      <MDBIcon className="mr-5" icon="upload" />
+                    </Button>
+                    <Button variant="outline-*" onClick={this.submitHandler}>
+                      <MDBIcon className="mr-5" icon="eye" />
+                    </Button>
 
-          <div className={mainClass.join(" ")}>
-            <div className="pcoded-main-container full-screenable-node">
-              <div className="pcoded-wrapper">
-                <div className="pcoded-content">
-                  <div className="pcoded-inner-content">
-                    <div className="main-body">
-                      <div className="page-wrapper">
-                        <Aux>
-                          <Row>
-                            <Col>
-                              <Card
-                                style={{ className: "bg-info text-white  " }}
-                              >
-                                <Card.Header>
-                                  <Card.Title>
-                                    {data.questionnaire_name}
-                                    <div className="d-flex justify-content-lg-end">
-                                      <Button variant="outline-*" disabled>
-                                        <MDBIcon
-                                          className="mr-5"
-                                          icon="upload"
-                                        />
-                                      </Button>
-                                      <Button
-                                        variant="outline-*"
-                                        onClick={this.submitHandler}
-                                      >
-                                        <MDBIcon className="mr-5" icon="eye" />
-                                      </Button>
-
-                                      <Button variant="outline-*" disabled>
-                                        <MDBIcon
-                                          className="mr-5"
-                                          icon="clone"
-                                        />
-                                      </Button>
-                                      <Button variant="outline-*" disabled>
-                                        <MDBIcon className="mr-5" icon="edit" />
-                                      </Button>
-                                      <Button variant="outline-*" disabled>
-                                        <MDBIcon
-                                          className="mr-5"
-                                          icon="trash-alt"
-                                        />
-                                      </Button>
-                                    </div>
-                                  </Card.Title>
-                                </Card.Header>
-                                <Card.Body>
-                                  <div
-                                    style={{ height: "500px" }}
-                                    className="p-3 mb-2 bg-info text-white"
-                                  >
-                                    <ul className="p-3 mb-2 bg-info  text-white">
-                                      <Row>
-                                        <Col>
-                                          <b>Created: </b> {data.creation_date}
-                                        </Col>
-                                        <Col>
-                                          <b>Language: </b>
-                                          {
-                                            {
-                                              1: "Arabic",
-                                              2: "English",
-                                              3: "Hebrew",
-                                              4: "Russian",
-                                            }[data.language_id]
-                                          }
-                                        </Col>
-                                      </Row>
-
-                                      <Row>
-                                        <Col>
-                                          <b>Hosted Link: </b>
-                                          {data.hosted_link}
-                                        </Col>
-                                        <Col>
-                                          <b>Status: </b>
-                                          {data.is_active !== undefined
-                                            ? [
-                                                data.is_active
-                                                  ? "True"
-                                                  : "False",
-                                              ]
-                                            : null}
-                                        </Col>
-                                      </Row>
-                                    </ul>
-                                  </div>
-                                </Card.Body>
-                              </Card>
-                            </Col>
-                          </Row>
-                        </Aux>
-                      </div>
-                    </div>
+                    <Button variant="outline-*" disabled>
+                      <MDBIcon className="mr-5" icon="clone" />
+                    </Button>
+                    <Button variant="outline-*" disabled>
+                      <MDBIcon className="mr-5" icon="edit" />
+                    </Button>
+                    <Button variant="outline-*" disabled>
+                      <MDBIcon className="mr-5" icon="trash-alt" />
+                    </Button>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
+                </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                <div style={{ height: "425px" }} className="bg-info text-white">
+                  <ul className="p-3 mb-2 text-white">
+                    <Row>
+                      <Col>
+                        <b>Created: </b> {data.creation_date}
+                      </Col>
+                      <Col>
+                        <b>Language: </b>
+                        {
+                          {
+                            1: "Arabic",
+                            2: "English",
+                            3: "Hebrew",
+                            4: "Russian",
+                          }[data.language_id]
+                        }
+                      </Col>
+                    </Row>
 
-          {/* <Configuration /> */}
+                    <Row>
+                      <Col>
+                        <b>Hosted Link: </b>
+                        {data.hosted_link}
+                      </Col>
+                      <Col>
+                        <b>Status: </b>
+                        {data.is_active !== undefined
+                          ? [data.is_active ? "True" : "False"]
+                          : null}
+                      </Col>
+                    </Row>
+                  </ul>
+                </div>
+              </Card.Body>
+            </Card>
+          </Aux>
         </nav>
       </Aux>
     );
