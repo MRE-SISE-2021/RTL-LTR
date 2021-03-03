727d4aacfbebceead0cacc56a4d285351a60a02c
mohsen <mohsen.abdalla.98@gmail.com>
2021-03-03 12:54:22 +0200

    delete question

--- Front/src/Components/QuestionsInput.js
+++ Front/src/Components/QuestionsInput.js
@@ -24,6 +24,24 @@ async function putData(url = "", data = {}) {
   return response.json(); // parses JSON response into native JavaScript objects
 }
 
+async function deleteData(url = "", data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: "follow", // manual, *follow, error
+    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
 class FormsElements extends React.Component {
   constructor(props) {
     super(props);
@@ -33,6 +51,7 @@ class FormsElements extends React.Component {
       label: "",
       id: props.expId,
       delete: true,
+      deleteAll: false,
       taskId: "",
     };
     this.onInputchange = this.onInputchange.bind(this);
@@ -67,7 +86,7 @@ class FormsElements extends React.Component {
       "http://127.0.0.1:8000/questionnaire-preview-data/" + this.state.id,
       response
     ).then((data) => {
-      console.log(data); // JSON data parsed by `data.json()` call
+      // console.log(data); // JSON data parsed by `data.json()` call
       this.setState({ taskId: data.task_id[0] });
     });
 
@@ -76,7 +95,31 @@ class FormsElements extends React.Component {
       delete: false,
     });
 
-    console.log(this.state);
+    // console.log(this.state);
+  };
+
+  deleteData = () => {
+    //PUT request -- save task
+    console.log(this.props);
+    const response = {
+      task_id: this.state.taskId, //
+    };
+
+    deleteData(
+      "http://127.0.0.1:8000/questionnaire-preview-data/" + this.state.id,
+      response
+    ).then((data) => {
+      console.log(data); // JSON data parsed by `data.json()` call
+      // this.setState({ taskId: data.task_id[0] });
+    });
+
+    ///show delete button
+    this.setState({
+      delete: true,
+      deleteAll: true,
+    });
+
+    // console.log(this.state);
   };
 
   onInputchange(event) {
@@ -86,7 +129,7 @@ class FormsElements extends React.Component {
   }
 
   render() {
-    // console.log(this.props);
+    // console.log(this.state);
     // const { validated, validatedTooltip } = this.state;
     let mainClass = ["content-main"];
     if (this.props.fullWidthLayout) {
@@ -120,6 +163,7 @@ class FormsElements extends React.Component {
                                   onChange={this.onInputchange}
                                   name="title"
                                   required
+                                  readOnly={this.state.deleteAll}
                                 />
                               </Card.Title>
                             </Card.Header>
@@ -134,6 +178,7 @@ class FormsElements extends React.Component {
                                       name="label"
                                       onChange={this.onInputchange}
                                       required
+                                      readOnly={this.state.deleteAll}
                                     />
                                   </Form.Group>
                                 </Col>
@@ -141,6 +186,7 @@ class FormsElements extends React.Component {
                                   <Button
                                     variant="primary"
                                     onClick={this.sendData}
+                                    disabled={this.state.deleteAll}
                                   >
                                     Save
                                   </Button>
@@ -149,6 +195,7 @@ class FormsElements extends React.Component {
                                   <Button
                                     variant="danger"
                                     disabled={this.state.delete}
+                                    onClick={this.deleteData}
                                   >
                                     Delete
                                   </Button>
@@ -163,6 +210,7 @@ class FormsElements extends React.Component {
                                         className="form-control-range"
                                         name="label"
                                         onChange={this.onInputchange}
+                                        readOnly={this.state.deleteAll}
                                       />
                                     </Form.Group>
                                   </Col>
